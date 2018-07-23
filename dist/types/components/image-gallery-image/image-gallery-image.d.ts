1a6aa5478070033e2694db3f044fb12e06f3cb26
Newton Munene <newtonmunene99@gmail.com>
2018-07-23 20:52:47 +0300

    CssFix

--- dist/types/components/image-gallery-image/image-gallery-image.d.ts
+++ dist/types/components/image-gallery-image/image-gallery-image.d.ts
@@ -86,6 +86,7 @@ import '../../stencil.core';
 import '../../stencil.core';
 import '../../stencil.core';
 import '../../stencil.core';
+import '../../stencil.core';
 export declare class ImageGalleryImage {
     comment: Boolean;
     render(): JSX.Element;
--- dist/types/components/image-gallery/image-gallery.d.ts
+++ dist/types/components/image-gallery/image-gallery.d.ts
@@ -7,6 +7,7 @@ import '../../stencil.core';
 import '../../stencil.core';
 import '../../stencil.core';
 import '../../stencil.core';
+import '../../stencil.core';
 export declare class ImageGallery {
     color: string;
     src: Array<any>;

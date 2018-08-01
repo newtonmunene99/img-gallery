23a37937acef45799813c870162797ab53104490
Newton Munene <newtonmunene99@gmail.com>
2018-08-01 11:06:04 +0300

    CssFix

--- dist/esm/es5/img-gallery.components.js
+++ dist/esm/es5/img-gallery.components.js
@@ -2,4 +2,4 @@
 
 export var ImgGallery = ["img-gallery",function(o){return(o.scoped?import("./cbfsq7v0.sc.js"):import("./cbfsq7v0.js")).then(function(m){return m.ImgGallery})},1,[["CurrentImageUrl",5],["color",1,0,1,2],["images",5],["preview",5],["src",1]],1];
 
-export var ImgGalleryImage = ["img-gallery-image",function(o){return(o.scoped?import("./zyqnqjol.sc.js"):import("./zyqnqjol.js")).then(function(m){return m.ImgGalleryImage})},1,[["alt",1,0,1,2],["el",7],["oldSrc",5],["src",1,0,1,2]],1];
+export var ImgGalleryImage = ["img-gallery-image",function(o){return(o.scoped?import("./48l1kvm2.sc.js"):import("./48l1kvm2.js")).then(function(m){return m.ImgGalleryImage})},1,[["alt",1,0,1,2],["el",7],["oldSrc",5],["src",1,0,1,2]],1];
